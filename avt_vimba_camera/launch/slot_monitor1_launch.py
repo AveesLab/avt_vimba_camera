d7ce7df0e3d94d26de2776e752523120127b3d15
kimhongsuk <ipeach0424@naver.com>
2023-06-15 21:10:10 +0900

    rewrite launch files

--- avt_vimba_camera/launch/slot_monitor1_launch.py
+++ avt_vimba_camera/launch/slot_monitor1_launch.py
@@ -62,8 +62,11 @@ def generate_launch_description():
             {"can_id": index + 101},
             {"time_interval": 1000},
             {"pcan_benchmark": False},
-            {"pcan_benchmark_start_stamp": 0.0},
-            {"pcan_benchmark_stamp_interval": 0.0},
+            {"pcan_benchmark_start_stamp": 10180.0},
+            {"pcan_benchmark_stamp_interval": 14.0},
+            {"max_camera_cycle_time": 33.5},
+            {"min_camera_cycle_time": 33.0},
+            {"convert_frame": 20}, 
         ]
     )
     
--- avt_vimba_camera/launch/slot_monitor_launch.py
+++ avt_vimba_camera/launch/slot_monitor_launch.py
@@ -64,6 +64,9 @@ def generate_launch_description():
             {"pcan_benchmark": False},
             {"pcan_benchmark_start_stamp": 10180.0},
             {"pcan_benchmark_stamp_interval": 14.0},
+            {"max_camera_cycle_time": 33.5},
+            {"min_camera_cycle_time": 33.0},
+            {"convert_frame": 20}, 
         ]
     )
     
