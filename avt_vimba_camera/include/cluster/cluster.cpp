aa309766f5c5fb669ca2c8b64cfb80babf762191
kimhongsuk <ipeach0424@naver.com>
2023-06-15 22:54:44 +0900

    fix min max

--- avt_vimba_camera/include/cluster/cluster.cpp
+++ avt_vimba_camera/include/cluster/cluster.cpp
@@ -34,8 +34,8 @@ bool ClusterManager::is_self_order(double timestamp)
     }
     else
     {
-      double min_base_timestamp = this->base_timestamp_ + this->max_camera_cycle_time_ * this->frame_interval_ * this->node_index_;
-      double max_base_timestamp = this->base_timestamp_ + this->min_camera_cycle_time_ * this->frame_interval_ * this->node_index_;
+      double min_base_timestamp = this->base_timestamp_ + this->min_camera_cycle_time_ * this->frame_interval_ * this->node_index_;
+      double max_base_timestamp = this->base_timestamp_ + this->max_camera_cycle_time_ * this->frame_interval_ * this->node_index_;
       return this->is_in_range(timestamp, min_base_timestamp, max_base_timestamp);
     }
   }
