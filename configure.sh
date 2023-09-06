8828d5b3f645db353c5c4650a5f071cedf36d771
kimhongsuk <51160500+kimhongsuk@users.noreply.github.com>
2023-09-06 20:58:12 +0900

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,3 +6,6 @@ mkdir ~/ros2_ws/weights
 
 cd ~/ros2_ws/src/clustering_sensor/avt_vimba_camera/launch
 sed -i "s/{NODE_ID}/${AVEES_CLUSTERING_NODE_ID}/g" computing_node.py
+
+cd ~/ros2_ws/src/clustering_sensor/avt_vimba_camera/src
+sed -i "s/{NODE_ID}/${AVEES_CLUSTERING_NODE_ID}/g" mono_camera_node.cpp
