8a8feb97c49b9721457312b00e72f80212924095
kimhongsuk <51160500+kimhongsuk@users.noreply.github.com>
2023-09-06 15:48:57 +0900

    fix bugs3 without test

--- requirements.sh
+++ requirements.sh
@@ -10,19 +10,19 @@ cd darknet
 git reset --hard fb2c3b0d6d25b7019af6c541e998d1716e56d7a7
 
 # Modify darknet.h for objectdetection.h
-if [ ! -e darknet/include/origin_darknet.h ]; then
-    mv darknet/include/darknet.h darknet/include/origin_darknet.h
+if [ ! -e include/origin_darknet.h ]; then
+    mv include/darknet.h darknet/include/origin_darknet.h
 fi
-cp darknet/include/origin_darknet.h darknet/include/darknet.h
-DARKNET_H_MODIFY=$(cat ../../../darknet_modify.txt | sed ':a;N;$!ba;s|\n|\\n|g' | sed 's|/|\\/|g' )
-sed -i "1112 a \\$DARKNET_H_MODIFY" darknet/include/darknet.h > darknet/include/darknet.h
+cp include/origin_darknet.h darknet/include/darknet.h
+DARKNET_H_MODIFY=$(cat ../../../../darknet_modify.txt | sed ':a;N;$!ba;s|\n|\\n|g' | sed 's|/|\\/|g' )
+sed -i "1112 a \\$DARKNET_H_MODIFY" include/darknet.h
 
 # Modify Makefile
-sed -i "s/GPU=0/GPU=1/g" darknet/Makefile > darknet/Makefile
-sed -i "s/OPENCV=0/OPENCV=1/g" darknet/Makefile > darknet/Makefile
-sed -i "s/LIBSO=0/LIBSO=1/g" darknet/Makefile > darknet/Makefile
-sed -i "60 a \\ARCH= -gencode arch=compute_72,code=[sm_72,compute_72]" darknet/Makefile > darknet/Makefile
-sed -i "61 a \\ARCH= -gencode arch=compute_72,code=[sm_87,compute_87]" darknet/Makefile > darknet/Makefile
+sed -i "s/GPU=0/GPU=1/g" Makefile
+sed -i "s/OPENCV=0/OPENCV=1/g" Makefile
+sed -i "s/LIBSO=0/LIBSO=1/g" Makefile
+sed -i "60 a \\ARCH= -gencode arch=compute_72,code=[sm_72,compute_72]" Makefile
+sed -i "61 a \\ARCH= -gencode arch=compute_72,code=[sm_87,compute_87]" Makefile
 
 # Build
 cd darknet
